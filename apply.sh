5384bc6d8a52aa98054f9f8f11810ac0b122fb10
richinfante <rich@richinfante.com>
2017-06-05 17:32:03 -0400

    Add method to fetch theme

--- apply.sh
+++ apply.sh
@@ -2,6 +2,13 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 echo "Copying dotfiles from \"$parent_path\""
 cd "$parent_path"
+
+# Copy theme from github
+git clone git@github.com:/tomasr/molokai
+mkdir -p ~/.vim
+mkdir -p ~/.vim/colors
+cp molokai/colors/molokai.vim ~/.vim/colors
+
 cp ./.hyper.js ~/.hyper.js
 cp ./.vimrc ~/.vimrc
 cp ./.bash_profile ~/.bash_profile
