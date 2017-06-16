b239285208cf0aa439cc63ce751e242da574fd1f
Rich Infante <richinfante@users.noreply.github.com>
2017-06-15 20:50:08 -0400

    Remove auto-pull

--- apply.sh
+++ apply.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
-echo "Copying dotfiles from \"$parent_path\""
+echo "[+] Copying dotfiles from \"$parent_path\""
 cd "$parent_path"
 
 # Clone theme from github (or just pull it)
-git clone git@github.com:/tomasr/molokai 2> /dev/null || (cd molokai && git pull)
+if [ ! -d molokai ]; then
+    git clone git@github.com:/tomasr/molokai 2> /dev/null
+else
+    echo "[+] Molokai theme already installed"
+fi
 mkdir -p ~/.vim
 mkdir -p ~/.vim/colors
 cp molokai/colors/molokai.vim ~/.vim/colors
