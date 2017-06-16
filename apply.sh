4fc4276894ab48bc973f31e366c68b6d50d64da4
Rich Infante <richinfante@users.noreply.github.com>
2017-06-15 20:48:01 -0400

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -3,8 +3,8 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 echo "Copying dotfiles from \"$parent_path\""
 cd "$parent_path"
 
-# Copy theme from github
-git clone git@github.com:/tomasr/molokai
+# Clone theme from github (or just pull it)
+git clone git@github.com:/tomasr/molokai 2> /dev/null || (cd molokai && git pull)
 mkdir -p ~/.vim
 mkdir -p ~/.vim/colors
 cp molokai/colors/molokai.vim ~/.vim/colors
