0390db4ad5f210c05619f9dfd5b6291d0e8f37fc
Rich Infante <rich@richinfante.com>
2018-01-03 14:49:04 -0500

    Embed vim theme in project

--- apply.sh
+++ apply.sh
@@ -3,15 +3,9 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 echo "[+] Copying dotfiles from \"$parent_path\""
 cd "$parent_path"
 
-# Clone theme from github (or just pull it)
-if [ ! -d molokai ]; then
-    git clone git@github.com:/tomasr/molokai 2> /dev/null
-else
-    echo "[+] Molokai theme already installed"
-fi
 mkdir -p ~/.vim
 mkdir -p ~/.vim/colors
-cp molokai/colors/molokai.vim ~/.vim/colors
+cp molokai-vim/colors/molokai.vim ~/.vim/colors
 
 cp ./.tmux.conf ~/.tmux.conf
 cp ./.hyper.js ~/.hyper.js
